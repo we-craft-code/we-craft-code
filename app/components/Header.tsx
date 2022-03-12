adce9f66ffb1acf057d4c6be6101fcd23756a4d6
Vignesh <61013244+ckvignesh@users.noreply.github.com>
2022-03-12 21:47:17 +0530

    feat: 2: Twitter Button Added (#16)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,38 +1,31 @@
-<<<<<<< HEAD
-import { FaGithub, FaDiscord, FaReddit, FaTwitter } from 'react-icons/fa'
-=======
-import { FaGithub, FaDiscord, FaReddit } from 'react-icons/fa'
->>>>>>> b711824 (Feat: Add Reddit Discord GitHub buttons (#6))
+import { FaGithub, FaDiscord, FaReddit, FaTwitter } from "react-icons/fa";
 
 export default function Header() {
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignContent: 'center',
-        fontSize: '4.0rem',
-        gap: '20px',
-        padding: '30px 30px 30px 40px',
-        backgroundColor: 'white',
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignContent: "center",
+        fontSize: "4.0rem",
+        gap: "20px",
+        padding: "30px 30px 30px 40px",
+        backgroundColor: "white",
       }}
     >
       <a href="https://github.com/we-craft-code" target="_blank">
-        <FaGithub style={{ color: '#333' }} />
+        <FaGithub style={{ color: "#333" }} />
       </a>
       <a href="https://discord.com/invite/kp3NQNKW9J" target="_blank">
-        <FaDiscord style={{ color: '#5865F2' }} />
+        <FaDiscord style={{ color: "#5865F2" }} />
       </a>
       <a href="https://www.reddit.com/r/wecraftcode/" target="_blank">
-        <FaReddit style={{ color: '#ff4500' }} />
+        <FaReddit style={{ color: "#ff4500" }} />
       </a>
-<<<<<<< HEAD
       <a href="https://twitter.com/we_craft_code" target="_blank">
-        <FaTwitter style={{ color: '#00acee' }} />
+        <FaTwitter style={{ color: "#00acee" }} />
       </a>
-=======
->>>>>>> b711824 (Feat: Add Reddit Discord GitHub buttons (#6))
     </div>
-  )
+  );
 }
