9eb34dc6512a94f939909a0ea4a79bd192dd7ed7
joeXimenes <dev@ximenes.me>
2022-03-13 20:05:32 +0000

    fix: remove img closing tag from body component

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,8 +1,7 @@
 export default function Body() {
   return (
     <div className="flex justify-center">
-      <img className="max-w-[80%]" src="/images/logo_transparent_background.png"
-      ></img>
+      <img className="max-w-[80%]" src="/images/logo_transparent_background.png" />
     </div>
   )
 }
