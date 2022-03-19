28d76ae23c59753a73450a4c3522c54420f3e748
joeXimenes <dev@ximenes.me>
2022-03-19 22:20:24 +0000

    feat: refactor and add background image

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,9 +1,16 @@
 export default function Body() {
   return (
-    <main className="flex justify-center flex-grow bg-cover">
-      <div className="container mx-auto">
-        <img src="/images/logo_transparent_background.png" />
-      </div>
-    </main>
+    <main className='bg-red-50 opacity-60 bg-scroll flex flex-grow bg-cover'>
+        <div className='flex flex-row container mx-auto'>
+          <section className='basis-1/2'>
+            <div>
+            </div>
+          </section>
+          <section className='basis-1/2'>
+            <div>
+            </div>
+          </section>
+        </div>
+      </main>
   )
 }
